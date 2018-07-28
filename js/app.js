418f6b7babde8516abf0df5f1835e961f3296f49
Bruno Campos <brunosopmac@gmail.com>
2018-07-28 20:14:03 -0300

    Randomize Function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,33 @@
-let cards = document.querySelectorAll('.card');
-let card = [...cards];
+const board = document.querySelectorAll('.card');
+const cards = [...board];
 
-console.log(card[0].childNodes);
+// randomize cards
+function shuffle(array) {
+    let control = array.length, tempValue, index;
+
+    while (control > 0) {
+        index = Math.floor(Math.random() * control);
+        control--;
+
+        tempValue = array[control];
+        array[control] = array[index];
+        array[index] = tempValue;
+    }
+
+    return array;
+}
+
+// deck all cards on deck
+const deck = document.querySelector('.deck');
+
+function start() {
+    let shuffledCards = shuffle(cards);
+
+    for (let i = 0; i < shuffledCards.length; i++) {
+        [].forEach.call(shuffledCards, function(item) {
+            deck.appendChild(item);
+        });
+    }
+}
+
+// the reset button
