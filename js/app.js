2e8488c1fc8a88764cac85f1286dc4538b232f82
Bruno Campos <brunosopmac@gmail.com>
2018-07-29 00:51:50 -0300

    Matched Cards Function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
+// collecting all cards on board
 const board = document.querySelectorAll('.card');
 const cards = [...board];
 
-// randomize cards
+// getting all cards on the deck
+const deck = document.querySelector('.deck');
+
+// openedCards init
+let openedCards = [];
+
+
+// displayCard is a function to change the card's states
+let displayCard = function() {
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+    this.classList.toggle('disabled');
+}
+
+
+// randomize cards position
 function shuffle(array) {
     let control = array.length, tempValue, index;
 
@@ -18,8 +34,6 @@ function shuffle(array) {
 }
 
 // deck all cards on deck
-const deck = document.querySelector('.deck');
-
 function start() {
     let shuffledCards = shuffle(cards);
 
@@ -30,4 +44,38 @@ function start() {
     }
 }
 
-// the reset button
+document.body.onload = start();
+
+// adding opened cards to a list and check if they are match or not
+function cardOpen() {
+    openedCards.push(this);
+
+    let len = openedCards.length;
+
+    if (len === 2) {
+        if (openedCards[0].dataset.type === openedCards[1].dataset.type) {
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+}
+
+// when the cards data-type match
+function matched() {
+    openedCards[0].classList.add('match', 'disabled');
+    openedCards[1].classList.add('match', 'disabled');
+    openedCards[0].classList.remove('show', 'open', 'no-event');
+    openedCards[1].classList.remove('show', 'open', 'no-event');
+    openedCards = [];
+}
+
+
+// looping and adding event listeners to each card
+for (let i = 0; i < cards.length; i++) {
+    card = cards[i];
+    card.addEventListener('click', displayCard);
+    card.addEventListener('click', cardOpen);
+    
+    console.log(cards[i]);
+}
